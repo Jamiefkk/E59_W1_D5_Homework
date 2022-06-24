ed12f996a0258df4217fb676e3b4bf5d68ef0a27
Jamie <jamiefogartykk@gmail.com>
2022-06-24 13:18:21 +0100

    Finished Q7

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -1,1 +1,15 @@
-# WRITE YOUR FUNCTIONS HERE
+def get_pet_shop_name(pet_name):
+    return pet_name["name"]
+
+def get_total_cash(admin_cash):
+    return admin_cash["admin"]["total_cash"]
+    
+def add_or_remove_cash(admin_cash, money):
+    admin_cash["admin"]["total_cash"] += money
+    
+def get_pets_sold(sold):
+    return sold["admin"]["pets_sold"]
+    
+def increase_pets_sold(more_pets, sold):
+    more_pets["admin"]["pets_sold"] += sold
+
