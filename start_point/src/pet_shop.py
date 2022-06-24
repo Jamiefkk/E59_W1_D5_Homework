6ddf5e580d618ab25f7120dcdf6b307bb5159470
Jamie <jamiefogartykk@gmail.com>
2022-06-24 13:32:53 +0100

    Finished Q8

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -13,3 +13,23 @@ def get_pets_sold(sold):
 def increase_pets_sold(more_pets, sold):
     more_pets["admin"]["pets_sold"] += sold
 
+def get_stock_count(pet_stock):
+    return len(pet_stock["pets"])
+
+def get_pets_by_breed(pets, breed):
+    count = 0
+    for type in pets:
+        if pets["pets"]["breed"] == breed:
+            count = count + 1
+    return count
+
+
+def get_pets_by_breed(pets, breed):
+    count = 0
+    for num_of in pets["pets"]["breed"]:
+        if num_of == breed:
+            count += 1
+    return count
+
+I can't hear anything from slack
+Did you allow slacks to access microphont? or Audio
